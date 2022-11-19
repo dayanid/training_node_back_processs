6af1d7df9c2dc09647711c67e33d14971ae14503
dayanidi <93707264+dayanid@users.noreply.github.com>
2022-11-19 12:27:54 +0530

    Update geturljsondata.js

--- geturljsondata.js
+++ geturljsondata.js
@@ -5,18 +5,15 @@ let url = "http://localhost:3000/users/paginate?page=1&limit=10";
 let options = {json: true};
 
 
-
-request(url, options, (error, res, body) => {
+//send request to url..
+request(url, options, (error, res,data) => {
     if (error) {
         return  console.log(error)
     };
 
     if (!error && res.statusCode == 200) {
-		var users = JSON.parse(JSON.stringify(body));
-		console.log(body.forEach((data)=>{
-							console.log(data);
-						})
-		);
-        // do something with JSON, using the 'body' variable
+		//var users = JSON.parse(JSON.stringify(data));
+		console.log(data);
+    
     };
 })
